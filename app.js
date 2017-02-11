98250ee36d67740ec045e24e18ba5756c8c74e9f
dbai18 <dbai18@gmail.com>
2017-02-11 14:47:40 -0800

    html framework, js logic completed

--- app.js
+++ app.js
@@ -1,185 +1,230 @@
-var timeLabelsArray = ['10AM: ','11AM: ','12PM: ', '1PM: ', '2PM: ', '3PM: ', '4PM: ', '5PM: ', 'SUM: ']
-var pikePlace = {
-  min: 17,
-  max: 88,
-  avg: 5.2,
-  randomOutput: function(){
-    return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
-  },
-  dailyRandomOutput: function(){
-    var cookiesArray = [];
-    for(i = 0; i < 8; i++){
-      var arrayValue = this.randomOutput();
-      cookiesArray.push(arrayValue);
-    }
-    return cookiesArray;
-  },
-  randomOutputSum: function(){
-    var arraySum = this.dailyRandomOutput();
-    var sum = 0;
-    for(i = 0; i < arraySum.length; i++){
-      sum += arraySum[i];
-    }
-    arraySum.push(sum);
-    console.log(arraySum);
-    return arraySum;
-  },
-  renderManipulation: function(){
-    var arrSum = this.randomOutputSum();
-    var x = document.getElementById('pike_place');
-    for(i=0;i<9;i++){
-      var el = document.createElement('li');
-      var updatedListItem = x.appendChild(el);
-      updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
-    };
-  }
-};
-pikePlace.renderManipulation();
+// var timeLabelsArray = ['10AM: ','11AM: ','12PM: ', '1PM: ', '2PM: ', '3PM: ', '4PM: ', '5PM: ', 'SUM: ']
 
-var seaTacAirport = {
-  min: 6,
-  max: 24,
-  avg: 1.2,
-  randomOutput: function(){
+function Store(name, min, max, avg){
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.randomOutput = function(){
     return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
-  },
-  dailyRandomOutput: function(){
+  };
+  this.dailyRandomOutput = function(){
     var cookiesArray = [];
     for(i = 0; i < 8; i++){
       var arrayValue = this.randomOutput();
       cookiesArray.push(arrayValue);
     }
     return cookiesArray;
-  },
-  randomOutputSum: function(){
-    var arraySum = this.dailyRandomOutput();
-    var sum = 0;
-    for(i = 0; i < arraySum.length; i++){
-      sum += arraySum[i];
-    }
-    arraySum.push(sum);
-    console.log(arraySum);
-    return arraySum;
-  },
-  renderManipulation: function(){
-    var arrSum = this.randomOutputSum();
-    var x = document.getElementById('seatac_airport');
-    for(i=0;i<9;i++){
-      var el = document.createElement('li');
-      var updatedListItem = x.appendChild(el);
-      updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
+  };
+  this.randomOutputSum = function(){
+      var arraySum = this.dailyRandomOutput();
+      var sum = 0;
+      for(i = 0; i < arraySum.length; i++){
+        sum += arraySum[i];
+      }
+      arraySum.push(sum);
+      console.log(arraySum);
+      return arraySum;
     };
+  this.storeLabel = function(){
+    var el = document.getElementById('name');
+    var tableRowElement = document.createElement('td');
+    var updateTableRow = el.appendChild(tableRowElement);
+    updateTableRow.textContent = this.name;
   }
-};
-seaTacAirport.renderManipulation();
+  this.renderTable = function(){
+    var outputArr = this.randomOutputSum();
+    console.log(outputArr);
+    for(i=0; i<outputArr.length; i++){
+      var el = document.getElementById('index' + i);
+      var tableRowElement = document.createElement('td');
+      var updateTableRow = el.appendChild(tableRowElement);
+      updateTableRow.textContent = outputArr[i];
+    }
+  }
+}
 
-var southcenter = {
-  min: 11,
-  max: 38,
-  avg: 1.9,
-  randomOutput: function(){
-    return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
-  },
-  dailyRandomOutput: function(){
-    var cookiesArray = [];
-    for(i = 0; i < 8; i++){
-      var arrayValue = this.randomOutput();
-      cookiesArray.push(arrayValue);
-    }
-    return cookiesArray;
-  },
-  randomOutputSum: function(){
-    var arraySum = this.dailyRandomOutput();
-    var sum = 0;
-    for(i = 0; i < arraySum.length; i++){
-      sum += arraySum[i];
-    }
-    arraySum.push(sum);
-    console.log(arraySum);
-    return arraySum;
-  },
-  renderManipulation: function(){
-    var arrSum = this.randomOutputSum();
-    var x = document.getElementById('southcenter');
-    for(i=0;i<9;i++){
-      var el = document.createElement('li');
-      var updatedListItem = x.appendChild(el);
-      updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
-    };
-  }
-};
-southcenter.renderManipulation();
-
-var bellevueSquare = {
-  min: 20,
-  max: 48,
-  avg: 3.3,
-  randomOutput: function(){
-    return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
-  },
-  dailyRandomOutput: function(){
-    var cookiesArray = [];
-    for(i = 0; i < 8; i++){
-      var arrayValue = this.randomOutput();
-      cookiesArray.push(arrayValue);
-    }
-    return cookiesArray;
-  },
-  randomOutputSum: function(){
-    var arraySum = this.dailyRandomOutput();
-    var sum = 0;
-    for(i = 0; i < arraySum.length; i++){
-      sum += arraySum[i];
-    }
-    arraySum.push(sum);
-    console.log(arraySum);
-    return arraySum;
-  },
-  renderManipulation: function(){
-    var arrSum = this.randomOutputSum();
-    var x = document.getElementById('bellevue_square');
-    for(i=0;i<9;i++){
-      var el = document.createElement('li');
-      var updatedListItem = x.appendChild(el);
-      updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
-    };
-  }
-};
-bellevueSquare.renderManipulation();
-
-var alki = {
-  min: 3,
-  max: 24,
-  avg: 2.6,
-  randomOutput: function(){
-    return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
-  },
-  dailyRandomOutput: function(){
-    var cookiesArray = [];
-    for(i = 0; i < 8; i++){
-      var arrayValue = this.randomOutput();
-      cookiesArray.push(arrayValue);
-    }
-    return cookiesArray;
-  },
-  randomOutputSum: function(){
-    var arraySum = this.dailyRandomOutput();
-    var sum = 0;
-    for(i = 0; i < arraySum.length; i++){
-      sum += arraySum[i];
-    }
-    arraySum.push(sum);
-    console.log(arraySum);
-    return arraySum;
-  },
-  renderManipulation: function(){
-    var arrSum = this.randomOutputSum();
-    var x = document.getElementById('alki');
-    for(i=0;i<9;i++){
-      var el = document.createElement('li');
-      var updatedListItem = x.appendChild(el);
-      updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
-    };
-  }
-};
-alki.renderManipulation();
+// var pikePlace = {
+//   min: 17,
+//   max: 88,
+//   avg: 5.2,
+//   randomOutput: function(){
+//     return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
+//   },
+//   dailyRandomOutput: function(){
+//     var cookiesArray = [];
+//     for(i = 0; i < 8; i++){
+//       var arrayValue = this.randomOutput();
+//       cookiesArray.push(arrayValue);
+//     }
+//     return cookiesArray;
+//   },
+//   randomOutputSum: function(){
+//     var arraySum = this.dailyRandomOutput();
+//     var sum = 0;
+//     for(i = 0; i < arraySum.length; i++){
+//       sum += arraySum[i];
+//     }
+//     arraySum.push(sum);
+//     console.log(arraySum);
+//     return arraySum;
+//   },
+//   renderManipulation: function(){
+//     var arrSum = this.randomOutputSum();
+//     var x = document.getElementById('pike_place');
+//     for(i=0;i<9;i++){
+//       var el = document.createElement('li');
+//       var updatedListItem = x.appendChild(el);
+//       updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
+//     };
+//   }
+// };
+// pikePlace.renderManipulation();
+//
+// var seaTacAirport = {
+//   min: 6,
+//   max: 24,
+//   avg: 1.2,
+//   randomOutput: function(){
+//     return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
+//   },
+//   dailyRandomOutput: function(){
+//     var cookiesArray = [];
+//     for(i = 0; i < 8; i++){
+//       var arrayValue = this.randomOutput();
+//       cookiesArray.push(arrayValue);
+//     }
+//     return cookiesArray;
+//   },
+//   randomOutputSum: function(){
+//     var arraySum = this.dailyRandomOutput();
+//     var sum = 0;
+//     for(i = 0; i < arraySum.length; i++){
+//       sum += arraySum[i];
+//     }
+//     arraySum.push(sum);
+//     console.log(arraySum);
+//     return arraySum;
+//   },
+//   renderManipulation: function(){
+//     var arrSum = this.randomOutputSum();
+//     var x = document.getElementById('seatac_airport');
+//     for(i=0;i<9;i++){
+//       var el = document.createElement('li');
+//       var updatedListItem = x.appendChild(el);
+//       updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
+//     };
+//   }
+// };
+// seaTacAirport.renderManipulation();
+//
+// var southcenter = {
+//   min: 11,
+//   max: 38,
+//   avg: 1.9,
+//   randomOutput: function(){
+//     return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
+//   },
+//   dailyRandomOutput: function(){
+//     var cookiesArray = [];
+//     for(i = 0; i < 8; i++){
+//       var arrayValue = this.randomOutput();
+//       cookiesArray.push(arrayValue);
+//     }
+//     return cookiesArray;
+//   },
+//   randomOutputSum: function(){
+//     var arraySum = this.dailyRandomOutput();
+//     var sum = 0;
+//     for(i = 0; i < arraySum.length; i++){
+//       sum += arraySum[i];
+//     }
+//     arraySum.push(sum);
+//     console.log(arraySum);
+//     return arraySum;
+//   },
+//   renderManipulation: function(){
+//     var arrSum = this.randomOutputSum();
+//     var x = document.getElementById('southcenter');
+//     for(i=0;i<9;i++){
+//       var el = document.createElement('li');
+//       var updatedListItem = x.appendChild(el);
+//       updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
+//     };
+//   }
+// };
+// southcenter.renderManipulation();
+//
+// var bellevueSquare = {
+//   min: 20,
+//   max: 48,
+//   avg: 3.3,
+//   randomOutput: function(){
+//     return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
+//   },
+//   dailyRandomOutput: function(){
+//     var cookiesArray = [];
+//     for(i = 0; i < 8; i++){
+//       var arrayValue = this.randomOutput();
+//       cookiesArray.push(arrayValue);
+//     }
+//     return cookiesArray;
+//   },
+//   randomOutputSum: function(){
+//     var arraySum = this.dailyRandomOutput();
+//     var sum = 0;
+//     for(i = 0; i < arraySum.length; i++){
+//       sum += arraySum[i];
+//     }
+//     arraySum.push(sum);
+//     console.log(arraySum);
+//     return arraySum;
+//   },
+//   renderManipulation: function(){
+//     var arrSum = this.randomOutputSum();
+//     var x = document.getElementById('bellevue_square');
+//     for(i=0;i<9;i++){
+//       var el = document.createElement('li');
+//       var updatedListItem = x.appendChild(el);
+//       updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
+//     };
+//   }
+// };
+// bellevueSquare.renderManipulation();
+//
+// var alki = {
+//   min: 3,
+//   max: 24,
+//   avg: 2.6,
+//   randomOutput: function(){
+//     return Math.floor(((Math.random()*(this.max - this.min + 1)) + this.min) * this.avg);
+//   },
+//   dailyRandomOutput: function(){
+//     var cookiesArray = [];
+//     for(i = 0; i < 8; i++){
+//       var arrayValue = this.randomOutput();
+//       cookiesArray.push(arrayValue);
+//     }
+//     return cookiesArray;
+//   },
+//   randomOutputSum: function(){
+//     var arraySum = this.dailyRandomOutput();
+//     var sum = 0;
+//     for(i = 0; i < arraySum.length; i++){
+//       sum += arraySum[i];
+//     }
+//     arraySum.push(sum);
+//     console.log(arraySum);
+//     return arraySum;
+//   },
+//   renderManipulation: function(){
+//     var arrSum = this.randomOutputSum();
+//     var x = document.getElementById('alki');
+//     for(i=0;i<9;i++){
+//       var el = document.createElement('li');
+//       var updatedListItem = x.appendChild(el);
+//       updatedListItem.textContent = timeLabelsArray[i] + arrSum[i];
+//     };
+//   }
+// };
+// alki.renderManipulation();
